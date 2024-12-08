66d01234962afb6696b3604465c003fe089be3b2
Dmitriy Kalyakin <dmitriykalyakin@Dmitriys-MacBook-Pro.local>
2024-12-08 17:39:09 +0300

    init proj

---
+++ Scrabble/ScrabbleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrabbleApp.swift
+//  Scrabble
+//
+//  Created by Dmitriy Kalyakin on 8/12/24.
+//
+
+import SwiftUI
+
+@main
+struct ScrabbleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
